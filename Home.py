2fd5e0a5d33200bf85fe4991510402e18513914c
Behnvm <behnamkhorsandian@gmail.com>
2023-11-04 16:21:50 +0400

    v0.0.0 - first commit

---
+++ Home.py
@@ -0,0 +1,22 @@
+# STREAMLIT
+import streamlit as st
+from streamlit_extras.grid import grid
+from streamlit_extras.app_logo import add_logo
+
+
+# --- PAGE CONFIG (BROWSER TAB) ---
+st.set_page_config(page_title="GoPlus", page_icon=":robot_face:", layout="centered", initial_sidebar_state="expanded")
+
+
+# ---- LOAD ASSETS ----
+def local_css(file_name):
+    with open(file_name) as f:
+        st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
+local_css("style/style.css")
+
+
+# --- MAIN PAGE ---
+with st.container():
+        st.title("Fuck Banks")
+        st.info("Nothing to see here!")
+        pass
---
+++ README.md
@@ -0,0 +1,6 @@
+[![Version](https://img.shields.io/badge/Version-0.0.1-blue)](https://sharktank.streamlit.app)
+[![Status](https://img.shields.io/badge/Status-Under_revision-yellow)](https://sharktank.streamlit.app)
+[![Revision](https://img.shields.io/badge/Last_Edit-03_Nov_2023-green)](https://sharktank.streamlit.app)
+
+# Sharktank 🦈
+
